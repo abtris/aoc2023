11cdc1f3229601e6dd423c5f0f0782b508138511
Ladislav Prskavec <ladislav@prskavec.net>
2023-12-01 08:57:58 +0100

    Day 1 part 2 (with help)

--- day01/main.go
+++ day01/main.go
@@ -17,6 +17,14 @@ func parseLine(line string) []string {
 	return re.FindAllString(line, -1)
 }
 
+var numbersInText = []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+
+var replacement = map[string]string{"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9", "1": "1", "2": "2", "3": "3", "4": "4", "5": "5", "6": "6", "7": "7", "8": "8", "9": "9"}
+
+func getIndex(line string, value string) int {
+	return strings.Index(line, value)
+}
+
 func prepareData(filename string) int {
 	file, err := os.Open(filename)
 	if err != nil {
@@ -27,26 +35,7 @@ func prepareData(filename string) int {
 	data := 0
 	for scanner.Scan() {
 		s := scanner.Text()
-		line := parseLine(s)
-		numbers := strings.Join(line, "")
-		if len(numbers) > 1 {
-			charAtString := fmt.Sprintf("%c%c", numbers[0], numbers[len(numbers)-1])
-			lineNumber, err := strconv.Atoi(charAtString)
-			if err != nil {
-				fmt.Println(err)
-			}
-			fmt.Println("x:", lineNumber)
-			data += lineNumber
-		} else {
-			number := fmt.Sprintf("%c%c", numbers[0], numbers[0])
-			lineNumber, err := strconv.Atoi(number)
-			if err != nil {
-				fmt.Println(err)
-			}
-			fmt.Println("y", lineNumber)
-			data += lineNumber
-		}
-
+		data += parseLine_part2(s)
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
@@ -54,6 +43,49 @@ func prepareData(filename string) int {
 	return data
 }
 
+func parseLine_part2(line string) int {
+	searches := []struct {
+		name string
+		val  string
+	}{{name: "one", val: "1"},
+		{name: "two", val: "2"},
+		{name: "three", val: "3"},
+		{name: "four", val: "4"},
+		{name: "five", val: "5"},
+		{name: "six", val: "6"},
+		{name: "seven", val: "7"},
+		{name: "eight", val: "8"},
+		{name: "nine", val: "9"},
+		{name: "1", val: "1"},
+		{name: "2", val: "2"},
+		{name: "3", val: "3"},
+		{name: "4", val: "4"},
+		{name: "5", val: "5"},
+		{name: "6", val: "6"},
+		{name: "7", val: "7"},
+		{name: "8", val: "8"},
+		{name: "9", val: "9"}}
+
+	var first, last string
+	for i := 0; i < len(line); i++ {
+		for _, search := range searches {
+			if strings.HasPrefix(line[i:], search.name) {
+				if first == "" {
+					first = search.val
+					last = search.val
+				} else {
+					last = search.val
+				}
+			}
+		}
+	}
+
+	calibration, _ := strconv.Atoi(string(first) + string(last))
+	return calibration
+}
+
 func main() {
+	// 53221 valid
+	// 53188 to low
 	fmt.Println(prepareData("input"))
 }
--- day01/main_test.go
+++ day01/main_test.go
@@ -1,1 +1,28 @@
 package main
+
+import "testing"
+
+func TestParseLine(t *testing.T) {
+	tests := []struct {
+		name   string
+		input  string
+		result string
+	}{
+		{name: "eighthree", input: "eighthree", result: "83"},
+		{name: "sevenine", input: "sevenine", result: "79"},
+		{name: "eightwothree", input: "eightwothree", result: "83"},
+		{name: "abcone2threexyz", input: "abcone2threexyz", result: "13"},
+		{name: "xtwone3four", input: "xtwone3four", result: "24"},
+		{name: "4nineeightseven2", input: "4nineeightseven2", result: "42"},
+		{name: "zoneight234", input: "zoneight234", result: "14"},
+		{name: "7pqrstsixteen", input: "7pqrstsixteen", result: "76"},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := parseLine_part2(test.input)
+			if result != test.result {
+				t.Errorf("Expected %v and real %v)", test.result, result)
+			}
+		})
+	}
+}
